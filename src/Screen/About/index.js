3e944504e692bf118f3d7702a1a863ecaf91958e
Sunil Kumawat <98253403+sunilkumawat6268@users.noreply.github.com>
2024-03-15 21:43:43 +0530

    Related commit(s): About Screen

---
+++ src/Screen/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const About = () => {
+    return (
+        <View>
+            <Text>Hello in the world in About Screen</Text>
+        </View>
+    )
+}
+
+export default About;
