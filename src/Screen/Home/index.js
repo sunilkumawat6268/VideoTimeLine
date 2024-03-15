238ea70ecfda0604c2a0f10591a38f6019ccc320
Sunil Kumawat <98253403+sunilkumawat6268@users.noreply.github.com>
2024-03-15 21:40:27 +0530

    Related commit(s): Add Home Screen

---
+++ src/Screen/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hello in the world in Home Screen</Text>
+        </View>
+    )
+}
+
+export default Home;
