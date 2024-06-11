79942e93b1804f3e269a9bb71ebd1cd92bea030e
Sunil Kumawat <98253403+sunilkumawat6268@users.noreply.github.com>
2024-06-11 20:56:46 +0530

    Related commit(s): code Optmize

--- src/Screen/QRCodeScanner/index.js
+++ src/Screen/QRCodeScanner/index.js
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 
 const QRCodeScanner = () => {
   return (
     <View>
       <Text>hello in the world of QR code </Text>
+      <Text>hello in the world of QR code </Text>
+      <Text>hello in the world of QR code </Text>
     </View>
-  )
-}
+  );
+};
 
-export default QRCodeScanner
+export default QRCodeScanner;
